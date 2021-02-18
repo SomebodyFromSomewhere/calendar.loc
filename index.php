cb5b092c56e21308c0e7bc54c3ea17cf05ba58c3
no487 <andreev@gmail.com>
2021-02-18 19:17:14 +0300

    Side menu done

--- index.php
+++ index.php
@@ -3,14 +3,35 @@
 <head>
     <title>index</title>
     <link href="lib/css/main.css" type="text/css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-  <div class="side_block" id="side_block">
-  </div>
-  <button class="btn" id="side_btn">click</button>
-  <div class="main">
+<div class="side_block" id="side_block">
+    <div class="profile">
+        <img class="user-avatar" src="lib/images/avatars/def0.jpg">
+        <a class="side_menu_link">Profile</a>
+        <a class="side_menu_link">Exit</a>
+    </div>
+    <a href="/" class="side_menu_link">Some Link</a>
+    <a href="/" class="side_menu_link">Some Link</a>
+    <a href="/" class="side_menu_link">Some Link</a>
+    <a href="/" class="side_menu_link">Some Link</a>
+    <a href="/" class="side_menu_link">Some Link</a>
+    <a href="/" class="side_menu_link">Some Link</a>
+    <a href="/" class="side_menu_link">Some Link</a>
+    <a href="/" class="side_menu_link">Some Link</a>
+    <a href="/" class="side_menu_link">Some Link</a>
+    <a href="/" class="side_menu_link">Some Link</a>
+    <a href="/" class="side_menu_link">Some Link</a>
+    <a href="/" class="side_menu_link">Some Link</a>
+    <a href="/" class="side_menu_link">Some Link</a>
+</div>
+<button class="btn" id="side_btn">
+    <i class="fa fa-bars"></i>
+</button>
+<div class="main">
 
-  </div>
+</div>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="lib/js/script.js"></script>
 </body>
