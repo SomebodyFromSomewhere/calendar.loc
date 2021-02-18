4c8f411cf90c65122a99e3c746eb2437f62bf034
no487 <andreev@gmail.com>
2021-02-18 19:17:14 +0300

    Starting

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <title>index</title>
+    <link href="lib/css/main.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+  <div class="side_block" id="side_block">
+  </div>
+  <button class="btn" id="side_btn">click</button>
+  <div class="main">
+
+  </div>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="lib/js/script.js"></script>
+</body>
+</html>
